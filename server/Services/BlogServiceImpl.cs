6d72777b900da5ed258ac4aba2cd45e38b6c61db
Mike Evlantev <mikeev21@gmail.com>
2020-01-04 16:45:38 -0800

    Implemented serverside service override for CreateBlog. Init connection code to mongo db.

--- server/Services/BlogServiceImpl.cs
+++ server/Services/BlogServiceImpl.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Blog;
+using Grpc.Core;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,5 +13,22 @@ namespace server.Services
 {
     public class BlogServiceImpl : BlogServiceBase
     {
+        private static MongoClient _mongoClient = new MongoClient();
+        private static IMongoDatabase _db = _mongoClient.GetDatabase("grpcblog");
+        private static IMongoCollection<BsonDocument> _collection = _db.GetCollection<BsonDocument>("blogs");
+        public override Task<CreateBlogResponse> CreateBlog(CreateBlogRequest request, ServerCallContext context)
+        {
+            var blog = request.Blog;
+            BsonDocument doc = new BsonDocument("authorId", blog.AuthorId)
+                                            .Add("title", blog.Title)
+                                            .Add("content", blog.Content);
+
+            _collection.InsertOne(doc);
+            blog.Id = doc.GetValue("_id").ToString();
+            return Task.FromResult(new CreateBlogResponse()
+            {
+                Blog = blog
+            });
+        }
     }
 }
