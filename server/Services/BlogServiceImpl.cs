98ca48a68d78b6b20264aca2c40add6952e83f91
Mike Evlantev <mikeev21@gmail.com>
2020-01-04 17:25:51 -0800

    introduced db dependency for CRUD interactions

--- server/Services/BlogServiceImpl.cs
+++ server/Services/BlogServiceImpl.cs
@@ -13,17 +13,24 @@ namespace server.Services
 {
     public class BlogServiceImpl : BlogServiceBase
     {
-        private static MongoClient _mongoClient = new MongoClient();
-        private static IMongoDatabase _db = _mongoClient.GetDatabase("grpcblog");
-        private static IMongoCollection<BsonDocument> _collection = _db.GetCollection<BsonDocument>("blogs");
+        const string Blogs = "blogs";
+
+        private static IMongoDatabase _db;
+        private static IMongoCollection<BsonDocument> _blogs;
+        public BlogServiceImpl(IMongoDatabase db)
+        {
+            _db = db;
+            _blogs = db.GetCollection<BsonDocument>(Blogs);
+        }
         public override Task<CreateBlogResponse> CreateBlog(CreateBlogRequest request, ServerCallContext context)
         {
+
             var blog = request.Blog;
             BsonDocument doc = new BsonDocument("authorId", blog.AuthorId)
                                             .Add("title", blog.Title)
                                             .Add("content", blog.Content);
 
-            _collection.InsertOne(doc);
+            _blogs.InsertOne(doc);
             blog.Id = doc.GetValue("_id").ToString();
             return Task.FromResult(new CreateBlogResponse()
             {
