4ff85fd47492a0e9e10d2798574ceaeb6f8e72ad
Mike Evlantev <mikeev21@gmail.com>
2020-01-04 21:24:49 -0800

    introduced client dependency, Get blog by id implementation

--- client/Repositories/BlogRepository.cs
+++ client/Repositories/BlogRepository.cs
@@ -1,4 +1,5 @@
 ﻿using Blog;
+using Grpc.Core;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,9 +10,14 @@ namespace client.Repositories
 {
     public class BlogRepository
     {
-        public CreateBlogResponse CreateBlog(BlogService.BlogServiceClient client)
+        private static BlogService.BlogServiceClient _client;
+        public BlogRepository(BlogService.BlogServiceClient client)
         {
-            return client.CreateBlog(new CreateBlogRequest()
+            _client = client;
+        }
+        public CreateBlogResponse CreateBlog()
+        {
+            return _client.CreateBlog(new CreateBlogRequest()
             {
                 Blog = new Blog.Blog()
                 {
@@ -20,6 +26,20 @@ namespace client.Repositories
                     Content = "Hello world!"
                 }
             });
+        }
+
+        public GetBlogByIdResponse GetBlogById(string id)
+        {
+            try
+            {
+                return _client.GetBlogById(new GetBlogByIdRequest() { Id = id });
+            }
+            catch (RpcException ex)
+            {
+                Console.WriteLine(ex.Status.Detail);
+                throw;
+            }
+            
         }
     }
 }
